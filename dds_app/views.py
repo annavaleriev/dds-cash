12265e6f88194bdb96b68b5448945d4071650e88
annavaleriev <filenko.a@gmail.com>
2025-06-06 20:27:55 +0300

    [develop] Автоформатирование dds_app/views.py black'ом

--- dds_app/views.py
+++ dds_app/views.py
@@ -1,23 +1,48 @@
-# from rest_framework.viewsets import ModelViewSet
-#
-#
-# class CashFlowViewSet(ModelViewSet):
-#     queryset =
-#     serializer_class =
-#     filter_backends =
-#     filterset_class =
-#     permission_classes =
-#
-#
-# class StatusViewSet(ModelViewSet):
-#     queryset =
-#     serializer_class =
-#     permission_classes =
-#
-#
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from dds_app.models import Status, Type
+from dds_app.serializer import StatusSerializer
+
+
+class StatusViewSet(ModelViewSet):
+    """ViewSet для статусов"""
+
+    queryset = Status.objects.all().order_by("name")
+    serializer_class = StatusSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class TypeViewSet(ModelViewSet):
+    """ViewSet для типов"""
+
+    queryset = Type.objects.all().order_by("name")
+    serializer_class = StatusSerializer
+    permission_classes = [IsAuthenticated]
+
+
 # class SubcategoryViewSet(ModelViewSet):
+#     """ViewSet для подкатегорий"""
 #     queryset =
 #     serializer_class =
 #     filter_backends =
 #     filter_fields = ("categories_set")
+#     permission_classes =
+#
+#
+# class CategoryViewSet(ModelViewSet):
+#     """ViewSet для категорий"""
+#     queryset =
+#     serializer_class =
+#     filter_backends =
+#     filter_fields = ("subcategory_set")
+#     permission_classes =
+#
+#
+# class CashFlowViewSet(ModelViewSet):
+#     """ViewSet для денежных потоков"""
+#     queryset =
+#     serializer_class =
+#     filter_backends =
+#     filterset_class =
 #     permission_classes =
