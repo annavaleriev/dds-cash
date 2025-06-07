41bf40b229a7759b0c328f9dc0d6841b12164d74
annavaleriev <filenko.a@gmail.com>
2025-06-07 18:18:33 +0300

    тест

--- config/settings.py
+++ config/settings.py
@@ -11,22 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-import os
-import os
 from pathlib import Path
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
