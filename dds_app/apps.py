c29f2dcd45bb1a74f0fa824b90fe17ee0d97d924
annavaleriev <filenko.a@gmail.com>
2025-06-18 17:24:13 +0300

    [develop] Обновлены файлы проекта

--- dds_app/apps.py
+++ dds_app/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class DdsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dds_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dds_app"
+    verbose_name = "ДДС"
--- dds_app/views.py
+++ dds_app/views.py
@@ -1,11 +1,17 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from dds_app.models import Status, Type, Subcategory, Category, CashFlow
+from dds_app.models import CashFlow, Category, Status, Subcategory, Type
 from dds_app.permissions import IsOwnerOnly
-from dds_app.serializer import StatusSerializer, SubcategorySerializer, CategoryCreateSerializer, \
-    CategoryBaseSerializer, CashFlowSerializer
+from dds_app.serializer import (
+    CashFlowCreateSerializer,
+    CashFlowSerializer,
+    CategoryBaseSerializer,
+    CategoryCreateSerializer,
+    StatusSerializer,
+    SubcategorySerializer,
+)
 
 
 class StatusViewSet(ModelViewSet):
@@ -26,6 +32,7 @@ class TypeViewSet(ModelViewSet):
 
 class SubcategoryViewSet(ModelViewSet):
     """ViewSet для подкатегорий"""
+
     queryset = Subcategory.objects.all().order_by("name")
     serializer_class = SubcategorySerializer
     filter_backends = [DjangoFilterBackend]
@@ -35,6 +42,7 @@ class SubcategoryViewSet(ModelViewSet):
 
 class CategoryViewSet(ModelViewSet):
     """ViewSet для категорий"""
+
     queryset = Category.objects.all().order_by("name")
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["subcategory"]
@@ -42,24 +50,26 @@ class CategoryViewSet(ModelViewSet):
 
     def get_serializer_class(self):
         """Возвращает сериализатор в зависимости от действия"""
-        if self.action  in ["list", "retrieve"]:
+        if self.action in ["list", "retrieve"]:
             return CategoryBaseSerializer
         return CategoryCreateSerializer
 
 
-
 class CashFlowViewSet(ModelViewSet):
     """ViewSet для денежных потоков"""
+
     queryset = CashFlow.objects.all().order_by("created_at")
     serializer_class = CashFlowSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["status", "type", "category", "subcategory"]
     permission_classes = [IsAuthenticated, IsOwnerOnly]
 
+    def get_serializer_class(self):
+        serializer_class = super().get_serializer_class()
+        if self.request.method in SAFE_METHODS:
+            return serializer_class
+        return CashFlowCreateSerializer
+
     def get_queryset(self):
         """Возвращает данные только если он владелец"""
         return self.queryset.filter(user=self.request.user)
-
-    def perform_create(self, serializer):
-        """Сохраняет данные с текущим пользователем"""
-        serializer.save(user=self.request.user)
